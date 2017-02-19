5a6516364f94ff85fa2fc783b448b21fff5d70c2
ericatsydney <eric.g.x.tan@gmail.com>
2017-02-19 06:38:27 +0000

    Change the css framework to materialize.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,26 +3,35 @@
 <head>
   <meta charset="utf-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Laravel + React example app</title>
+  <title>My Baby Journal</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.0/css/mdb.min.css" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 
 <body>
   <!--Navbar-->
   <nav class="navbar navbar-toggleable-md navbar-dark unique-color">
-    <a class="navbar-brand" href="#">
-      <strong>My Baby Journal</strong>
-    </a>
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <strong>My Baby Journal</strong>
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav1">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" ref="/profiles/1">Profiles <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" ref="/profiles/6">Albums <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
   <div class="container">
     <div id="app" class="row"></div>
   </div>
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.0/js/mdb.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
   <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
