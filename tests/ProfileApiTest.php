07698861cf2ae3e272b18c02a83154b2499d582e
ericatsydney <eric.g.x.tan@gmail.com>
2017-04-18 21:57:05 +0000

    Add test case for api

---
+++ tests/ProfileApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProfileApiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testProfileList()
+    {
+      // Assert that each user in the list has at least an id, name and email attribute.
+      $this->get('/api/profiles')
+        ->seeJsonStructure([
+          '*' => [
+            'id', 'first_name', 'last_name', 'gender', 'date_of_birth'
+          ]
+        ]); 
+    }
+
+    public function testProfile()
+    {
+      // Assert that each user in the list has at least an id, name and email attribute.
+      $this->get('/api/profiles/2')
+        ->seeJsonStructure([
+          'id', 'first_name', 'last_name', 'gender', 'date_of_birth'
+        ]); 
+    }
+}
