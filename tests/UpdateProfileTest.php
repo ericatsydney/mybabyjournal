10b20e41c655965d503f45daeb46d2853af47a2d
ericatsydney <eric.g.x.tan@gmail.com>
2017-04-19 22:29:15 +0000

    Add profile update test case.

---
+++ tests/UpdateProfileTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UpdateProfileTest extends TestCase
+{
+    /**
+     * Test update profile functionality.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+      $this->assertTrue(true);
+      // Psudo code:
+      // Get first record of profile
+      // Retrieve the data and send a update request
+      // visit the profiles page again, make sure you can see the updated information
+    }
+}
